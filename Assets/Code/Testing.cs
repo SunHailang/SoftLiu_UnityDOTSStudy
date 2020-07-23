daa3a1f1f573a0f7f81d8b092bd293165ee09156
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-07-23 14:19:02 +0800

    add code Entity Component

---
+++ Assets/Code/Testing.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
+
+public class Testing : MonoBehaviour
+{
+
+    private void Start()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(LevelComponent),
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld)
+            );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(1, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+        for (int i = 0; i < entityArray.Length; i++)
+        {
+            Entity entity = entityArray[i];
+            entityManager.SetComponentData(entity, new LevelComponent() { level = UnityEngine.Random.Range(10, 20) });
+
+            entityManager.SetSharedComponentData(entity, new RenderMesh()
+            {
+               // mesh = 
+            });
+        }
+        entityArray.Dispose();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void ReallyToughTask()
+    {
+
+    }
+
+}
