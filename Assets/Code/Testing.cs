e5c0a2c2df66cd5a5b87dc4abf948a2aacac5190
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-08-05 15:11:08 +0800

    spaw entity

--- Assets/Code/Testing.cs
+++ Assets/Code/Testing.cs
@@ -5,6 +5,7 @@ using Unity.Entities;
 using Unity.Transforms;
 using Unity.Collections;
 using Unity.Rendering;
+using Unity.Mathematics;
 
 public class Testing : MonoBehaviour
 {
@@ -17,6 +18,15 @@ public class Testing : MonoBehaviour
     private void Start()
     {
         EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(1, Allocator.Temp);
+        //var entityGameObject = GameObjectConversionUtility.ConvertGameObjectHierarchy(m_prefab,
+        //    new GameObjectConversionSettings(World.DefaultGameObjectInjectionWorld, GameObjectConversionUtility.ConversionFlags.AssignName,
+        //    new BlobAssetStore()));
+        //Entity entity = entityManager.Instantiate(entityGameObject);
+        //entityManager.SetComponentData(entityGameObject, new Translation() { Value = float3.zero });
+
+
         EntityArchetype entityArchetype = entityManager.CreateArchetype(
             typeof(LevelComponent),
             typeof(Translation),
@@ -24,14 +34,14 @@ public class Testing : MonoBehaviour
             typeof(LocalToWorld)
             );
 
-        NativeArray<Entity> entityArray = new NativeArray<Entity>(1, Allocator.Temp);
+
         entityManager.CreateEntity(entityArchetype, entityArray);
         for (int i = 0; i < entityArray.Length; i++)
         {
             Entity entity = entityArray[i];
-            entityManager.SetComponentData(entity, new LevelComponent() { level = UnityEngine.Random.Range(10, 20) });
+            entityManager.SetComponentData(entity, new LevelComponent { level = UnityEngine.Random.Range(10, 20) });
 
-            entityManager.SetSharedComponentData(entity, new RenderMesh()
+            entityManager.SetSharedComponentData(entity, new RenderMesh
             {
                 mesh = m_mesh,
                 material = m_material,
---
+++ Assets/EntityPrefab/Code/EntitySpawnerSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class EntitySpawnerSystem : ComponentSystem
+{
+    private float m_spawnTimer = 0;
+    private Unity.Mathematics.Random random;
+
+    protected override void OnCreate()
+    {
+        random = new Unity.Mathematics.Random(56);
+    }
+
+    protected override void OnUpdate()
+    {
+        //m_spawnTimer -= UnityEngine.Time.deltaTime;
+        if (m_spawnTimer <= 0)
+        {
+            m_spawnTimer = 0.5f;
+            // Spawn
+
+            Entity spawnedEntity = EntityManager.Instantiate(PrefabEntities.prefabEntity);
+            EntityManager.SetComponentData(spawnedEntity, new Translation() { Value = new float3(random.NextFloat(-5, 5), random.NextFloat(-5, 5), 0) });
+
+            /*
+            Entities.ForEach((ref PrefabEntityComponent prefabEntityComponent) =>
+            {
+                Entity spawnedEntity = EntityManager.Instantiate(prefabEntityComponent.prefabEntuity);
+
+                EntityManager.SetComponentData(spawnedEntity,
+                    new Translation { Value = new float3(random.NextFloat(-5, 5), random.NextFloat(-5,5), 0)}
+                    );
+            });
+            */
+        }
+    }
+}
